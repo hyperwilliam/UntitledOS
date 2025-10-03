fb37ec73a6b5c5d39fe69ea8fddf9e938e55562f
Hyper <133247253+hyperwilliam@users.noreply.github.com>
2025-10-03 12:40:09 +0100

    Update boot.asm

--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -2,6 +2,25 @@
 
 call Start
 nop
+OEMLabel		db "UNTITLED"	; Disk label
+BytesPerSector		dw 512		; Bytes per sector
+SectorsPerCluster	db 1		; Sectors per cluster
+ReservedForBoot		dw 1		; Reserved sectors for boot record
+NumberOfFats		db 2		; Number of copies of the FAT
+RootDirEntries		dw 224		; Number of entries in root dir
+					; (224 * 32 = 7168 = 14 sectors to read)
+LogicalSectors		dw 2880		; Number of logical sectors
+MediumByte		db 0F0h		; Medium descriptor byte
+SectorsPerFat		dw 9		; Sectors per FAT
+SectorsPerTrack		dw 18		; Sectors per track (36/cylinder)
+Sides			dw 2		; Number of sides/heads
+HiddenSectors		dd 0		; Number of hidden sectors
+LargeSectors		dd 0		; Number of LBA sectors
+DriveNo			dw 0		; Drive No: 0
+Signature		db 41		; Drive signature: 41 for floppy
+VolumeID		dd 00000000h	; Volume ID: any number
+VolumeLabel		db "UNTITLEDOS "; Volume Label: any 11 chars
+FileSystem		db "FAT12   "	; File system type: don't change!
 
 
 
@@ -44,7 +63,7 @@ call bios_print
    call bios_print   
 
    mov ax, 0x4F02  ; VESA set mode function
-   mov bx, 0x4118  ; Mode number and LFB/DM flags
+   mov bx, 0x4107  ; Mode number and LFB/DM flags
    int 0x10        ; Call VESA BIOS
    cmp ax, 0x004F
    jne errorvesa

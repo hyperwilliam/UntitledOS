798199e6967f03a6814c3ddd7b895bf923d11719
Hyper <133247253+hyperwilliam@users.noreply.github.com>
2025-10-15 16:09:02 +0100

    Fixed Some Directory Issues

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 :: You Might Need MSYS For this
-nasm -f bin boot.asm -o boot.bin
-nasm -f bin kernel.asm -o kernel.bin
-dd if=/dev/zero of=kernel.img bs=512 count=2880
-dd if=boot.bin of=kernel.img conv=notrunc
-dd if=kernel.bin of=kernel.img seek=1 conv=notrunc
+mkdir build
+nasm -f bin src/bootloader/boot.asm -o build/boot.bin
+nasm -f bin src/kernel/kernel.asm -o build/kernel.bin
+dd if=/dev/zero of=build/kernel.img bs=512 count=2880
+dd if=build/boot.bin of=build/kernel.img conv=notrunc
+dd if=build/kernel.bin of=build/kernel.img seek=1 conv=notrunc
 qemu-system-i386 -fda kernel.img

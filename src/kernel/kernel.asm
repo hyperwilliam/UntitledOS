9ee66329adcef6664d5c04faae1d62d8f41ccc8f
Hyper <133247253+hyperwilliam@users.noreply.github.com>
2025-11-10 13:21:24 +0000

    Update kernel.asm
    Added PIC initialisation, Throws a UD Exception After Enabling Interrupts.

--- src/kernel/kernel.asm
+++ src/kernel/kernel.asm
@@ -1,5 +1,13 @@
 org 0x8000
 bits 16
+jmp main
+
+%include "src/kernel/idt.asm"
+; %include "src/kernel/shell.asm" (Shell Isnt Ready Yet!)
+
+bits 16
+
+main:
 
 mov si, msg
 call bios_print
@@ -27,12 +35,45 @@ protected_mode:
    mov ah, 0x0F
    lidt [idtr]
    call print32
-   int 0x00
-   jmp short $
+   mov al, 0x11
+   mov dx, 0x20
+   out dx, al
+   call iowait
+   mov al, 0x11
+   mov dx, 0xA0
+   out dx, al
+   call iowait
+   mov al, 0x20
+   out 0x21, al
+   call iowait
+   mov al, 0x30
+   out 0xA1, al
+   call iowait
+   mov al, 0x02
+   out 0x21, al
+   call iowait
+   mov al, 2
+   out 0xA1, al
+   call iowait
+   mov al, 1
+   out 0x21, al
+   call iowait
+   out 0xA1, al
+   call iowait
+   mov al, 0xFE
+   out 0x21, al
+   out 0xA1, al 
+   mov esi, pic
+   mov edi, 0xB8500
+   call print32
+   mov edi, 0xB85A0
+   sti
+   jmp $ ;shell isnt ready yet, also it gives a #UD Exception Not Long after enabling interrupts.
 
 
 string: db "32 Bit Mode!!!!", 0
-string2: db "UntitledOS Pre-Alpha Revision 3!", 0
+string2: db "UntitledOS Pre-Alpha Revision 5!", 0
+pic: db "PIC Initialised!", 0
 idt: db "IDT Loaded, Very Good!", 0
 idt2: db "Interrupts Work, Awesome!", 0
 print32:
@@ -42,14 +83,7 @@ print32:
    jnz print32
    ret
 
-print32:
-   lodsb
-   stosw
-   or al, al
-   jnz print32
-   ret
-
-; GDT, Taken From UntitledOS
+; GDT, Taken From UntitledOS Legacy Branch
 gdt_start:
     gdt_null:
     dd 0x0
@@ -81,26 +115,204 @@ gdt_descriptor:
 CODE_SEG equ 0x08
 DATA_SEG equ 0x10
 
-; IDT Stub
-idt_start:
-dw isr1
-dw 0x0008
-db 0x0
-db 0x8E
-dw 0x0000
-
-idt_end:
-
-idtr:
-    dw idt_end - idt_start - 1
-    dd idt_start
+isr0:
+call isrs
+mov esi, errorcode0
+call print32
+jmp short $
 
 isr1:
-   mov edi, 0xB8500
-   mov esi, idt2
-   mov ah, 0x0F
+call isrs
+mov esi, errorcode1
+call print32
+jmp short $
+
+
+isr2:
+call isrs
+mov esi, errorcode2
+call print32
+jmp short $
+
+
+isr3:
+mov esi, brkp
+call print32
+iret
+
+isr4:
+call isrs
+mov esi, errorcode4
+call print32
+jmp short $
+
+isr5:
+call isrs
+mov esi, errorcode5
+call print32
+
+isr6:
+call isrs
+mov esi, errorcode6
+call print32
+jmp short $
+
+isr7:
+call isrs
+mov esi, errorcode7
+call print32
+jmp short $
+
+isr8:
+call isrs
+mov esi, errorcode8
+call print32
+jmp short $
+
+isr9:
+call isrs
+mov esi, errorcode9
+call print32
+jmp short $
+
+isrA:
+call isrs
+mov esi, errorcodeA
+call print32
+jmp short $
+
+isrB:
+call isrs
+mov esi, errorcodeB
+call print32
+jmp short $
+
+isrC:
+call isrs
+mov esi, errorcodeC
+call print32
+jmp short $
+
+isrD:
+call isrs
+mov esi, errorcodeD
+call print32
+jmp short $
+
+isrE:
+call isrs
+mov esi, errorcodeE
+call print32
+jmp short $
+
+Reserved:
+call isrs
+mov esi, reserved
+call print32
+jmp short $
+
+isr10:
+call isrs
+mov esi, errorcode10
+call print32
+jmp short $
+
+isr11:
+call isrs
+mov esi, errorcode11
+call print32
+jmp short $
+
+isr12:
+call isrs
+mov esi, errorcode12
+call print32
+jmp short $
+
+isr13:
+call isrs
+mov esi, errorcode13
+call print32
+jmp short $
+
+isr14:
+call isrs
+mov esi, errorcode14
+call print32
+jmp short $
+
+isr15:
+call isrs
+mov esi, errorcode15
+call print32
+jmp short $
+
+irq0:
+iret
+
+iowait:
+push ax
+mov ax, 0x0000
+out 0x80, al
+pop ax
+ret
+
+isrs:
+   mov edi, 0xB8000
+   mov ah, 0x7F
+   mov esi, line
    call print32
-   iret
+   call drawerr
+   mov ah, 0x4F
+   mov edi, 0xB80A0
+   mov esi, err
+   call print32
+   mov esi, err2
+   add byte edi, 50
+   call print32
+   ret
+
+line: db "FATAL ERROR!                                                                   ", 0
+err: db "UntitledOS Ran Into A Problem, And It Needs To Reboot.", 0 ;
+err2: db "ERROR CODE:", 0
+unhandlederr: db "UNRECOGNISED", 0
+
+brkp: db "Breakpoint Interrupt Received!", 0
+errorcode0: db "#DE", 0
+errorcode1: db "#DB", 0
+errorcode2: db "NMI", 0
+errorcode4: db "#OF", 0
+errorcode5: db "#BR", 0
+errorcode6: db "#UD", 0
+errorcode7: db "#NM", 0
+errorcode8: db "#DF", 0
+errorcode9: db "CSO", 0
+errorcodeA: db "#TS", 0
+errorcodeB: db "#NP", 0
+errorcodeC: db "#SS", 0
+errorcodeD: db "#GP", 0
+errorcodeE: db "#PF", 0
+errorcode10: db "#MF", 0
+errorcode11: db "#AC", 0
+errorcode12: db "#MC", 0
+errorcode13: db "#XM", 0
+errorcode14: db "#VE", 0
+errorcode15: db "Control Protection", 0
+reserved:   db "Reserved Interrupt!", 0
+
+drawerr:
+   mov ah, 0x44
+   mov esi, line
+   call print32
+   add byte [counter], -1
+   jnz drawerr
+   mov ah, 0x77
+   mov esi, line
+   call print32
+   ret
+counter: db 23
+
+
 
 
 bios_print:

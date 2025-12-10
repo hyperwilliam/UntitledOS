53df51e5d8791c83bef70318aaa4aee5699cf37c
Hyper <133247253+hyperwilliam@users.noreply.github.com>
2025-12-10 16:44:23 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,9 @@
 mkdir build
 nasm -f bin src/bootloader/boot.asm -o build/boot.bin
 nasm -f bin src/kernel/kernel.asm -o build/kernel.bin
+nasm -f bin src/bootloader/boot2.asm -o build/boot2.bin
 dd if=/dev/zero of=build/kernel.img bs=512 count=2880
 dd if=build/boot.bin of=build/kernel.img conv=notrunc
-dd if=build/kernel.bin of=build/kernel.img seek=1 conv=notrunc
+dd if=build/boot2.bin of=build/kernel.img seek=1 conv=notrunc
+dd if=build/kernel.bin of=build/kernel.img seek=5 conv=notrunc
 qemu-system-i386 -fda build/kernel.img

156c92f77d2b11526560f5fcd5ff66eac93dcfae
Hyper <133247253+hyperwilliam@users.noreply.github.com>
2026-03-06 10:45:52 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 :: You Might Need MSYS For this
 mkdir build
-nasm -felf32 src/boot/boot.asm -o build/boot.o
+nasm -felf32 src/bootloader/boot.asm -o build/boot.o
 i686-elf-gcc -c src/kernel/kernel.c -o build/kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
 i686-elf-gcc -T src/link.ld -o myos -ffreestanding -O2 -nostdlib build/boot.o build/kernel.o -lgcc
 qemu-system-i386 -kernel myos
